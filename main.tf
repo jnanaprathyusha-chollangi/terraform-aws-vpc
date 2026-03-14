1483df2c14e31d1cf9327fa351e7287b49b576c8
prathyusha <chollangiprathyusha@gmail.com>
2026-03-14 14:43:11 +0530

    updating the avaliablity zones

--- main.tf
+++ main.tf
@@ -11,13 +11,19 @@ resource "aws_internet_gateway" "gw" {
   tags = local.igw_final_tags
 }
 
-/* # Public Subnets
+# Public Subnets
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
-
-  tags = {
-    Name = "Main"
-  }
-} */
+  availability_zone = local.azs_names[count.index]
+  map_public_ip_on_launch = true
+  tags  = merge(
+    local.common_tags,
+    #roboshop-dev-public-us-east-1a
+    {
+      Name = "${var.project}-${var.environment}-public-${local.azs_names[count.index]}"
+    },
+    var.public_subnets_tags
+  )
+} 
--- variable.tf
+++ variable.tf
@@ -29,3 +29,7 @@ variable "public_subnet_cidrs" {
     default = ["10.0.1.0/24","10.0.2.0/24"]
   
 }
+variable "public_subnets_tags" {
+    default = {}
+    type = list
+}
